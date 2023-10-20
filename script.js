9f247dba6e5cce3e6d561c7279bd39abc91f1f23
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-10-20 14:35:43 +0000

    basic quiz structure

--- script.js
+++ script.js
@@ -1,53 +1,95 @@
-document.addEventListener("DOMContentLoaded",function() {
-    const display = document.getElementById('display');
-
-});
-
+// DOM Elements
+    const quizContainer = document.getElementById('quiz-container');
+    const nextButton = document.getElementById('next-button');
 
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
-    {
-        question: "What is the capital of New Zealand?",
-        answers:{
-            a: "Madrid",
-            b: "Wellington",
-            c: "Cardiff",
-            d: "Berlin"
-        },
-        correctAnswer:"c"
-    },
-    {
-        question: "What was the most popular girls name in the UK in 2021?",
-        answers:{
-            a: "Jane",
-            b: "Maria",
-            c: "Millicent",
-            d: "Olivia"
-    },
-    correctAnswer:"d"
+// state variables
+
+let currentQuestionIndex = 0;
+let questions = [ {
+    question: "What is the capital of New Zealand?",
+    options: ['Madrid', 'Wellington', 'Cardiff', 'Berlin'],
+
+    correctAnswer: "Wellington"
+},
+
+{
+    question: "What is the most popular girl's name in 2023?",
+    options: ['Jane', 'Mary', 'Olivia', 'North'],
+
+    correctAnswer: "Olivia"
+}, ]
+
+// Event listener on click that will need to be there for every question
+
+nextButton.addEventListener('click', loadNextQuestion);
+
+// Initialize the Quiz
+initializeQuiz();
+
+// Initialize the Quiz by asking the first question
+
+function initializeQuiz() {
+    displayQuestion(
+        questions[currentQuestionIndex
+        ]
+    )
 }
-];
 
+function displayQuestion(questionObj) {
 
-function showQuestions() {
-    let output = [];
-    let answers;
-    for (let i=0; i<myQuestions.length; i++){
-        answers = [];
+    // empty current question 
+    quizContainer.innerHTML = '';
+
+    // create a h2 
+    const questionElement = document.createElement('h2');
+    // load the h2 with question text 
+    questionElement.innerText = questionObj.question;
+
+    //display question on the screen
+
+    quizContainer.appendChild(questionElement);
+    // display alll the options as buttons using the createOptionButton function:
+    questionObj.options.forEach(option => {
+        const button = createOptionButton(option)
+        quizContainer.appendChild(button);
+    });
+}
         
+// set the text of the button to be equal to option:
+function createOptionButton(option) {
+    const button = document.createElement('button');
+    button.innerText = option;
+    button.classList.add('btn', 'btn-outline-primary', 'mt-2');
+    button.addEventListener('click' ,() => handleOptionClick(option));
+    return button
+}
+
+
+// proceed to load next question
+
+function loadNextQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        displayQuestion(questions[currentQuestionIndex])
+    } else {
+        endQuiz();
     }
-}
-
-
-function buildQuiz () {
-
 
 }
 
-function showResults(){
+function endQuiz() {
+    quizContainer.innerHTML = '<h2> You lost!</h2>';
+    nextButton.classList.add('d-none');
+}
+
+function handleOptionClick(optionSelected) {
+    const correctAnswer = questions[currentQuestionIndex].correctAnswer;
+    if (optionSelected === correctAnswer) {
+        // alert
+        alert('Correct Answer');
+    } else {
+        alert('Incorrect Answer')
+    }
 
 }
-submitButton.addEventListener('click', showResults);
